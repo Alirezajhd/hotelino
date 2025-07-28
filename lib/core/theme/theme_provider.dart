b93103bbef86837b1b5b78fb3e273d6700c29527
Alirezajhd <alirezajahed30@gmail.com>
2025-07-28 20:24:16 +0330

    add theme_provider

---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  Brightness _brightness;
+  ThemeProvider(this._brightness);
+
+  Brightness get brightness => _brightness;
+
+  void updateBrightness(Brightness brightness) {
+    _brightness = brightness;
+    notifyListeners();
+  }
+
+  void togleBrightness(Brightness brightness) {
+    _brightness =
+        (brightness == Brightness.light) ? Brightness.dark : Brightness.light;
+    notifyListeners();
+  }
+}
