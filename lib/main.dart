b3e524d12cff8f49d6ddcd4abd4b531911288750
Alirezajhd <alirezajahed30@gmail.com>
2025-07-29 22:07:21 +0330

    add lazyBootstrap.dart and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:Hotelino/bootstrap.dart';
 import 'package:Hotelino/core/theme/app_theme.dart';
 import 'package:Hotelino/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await lazyBootstrap();
+  FlutterNativeSplash.remove();
+
   runApp(
     MultiProvider(
       providers: [
@@ -45,7 +52,10 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     super.didChangePlatformBrightness();
     final brightness =
         WidgetsBinding.instance.platformDispatcher.platformBrightness;
-    Provider.of<ThemeProvider>(context, listen: false).updateBrightness(brightness);
+    Provider.of<ThemeProvider>(
+      context,
+      listen: false,
+    ).updateBrightness(brightness);
   }
 
   @override
