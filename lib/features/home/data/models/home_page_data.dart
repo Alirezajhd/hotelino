5f1cf1d8ecea415ed589cbad9ce038e336302336
Alirezajhd <alirezajahed30@gmail.com>
2025-08-13 12:22:32 +0330

    add HomePageDataConstants

---
+++ lib/features/home/data/models/home_page_data.dart
@@ -0,0 +1,17 @@
+class HomePageData {
+  final List<String> favoirtes;
+  final List<String> discounted;
+  final List<String> recentlyViewed;
+  final List<String> popular;
+  final List<String> newest;
+  final List<String> specialOffers;
+
+  HomePageData({
+    required this.favoirtes,
+    required this.discounted,
+    required this.recentlyViewed,
+    required this.popular,
+    required this.newest,
+    required this.specialOffers,
+  });
+}
