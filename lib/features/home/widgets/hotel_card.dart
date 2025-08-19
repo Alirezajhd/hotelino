9d538d3a830b31c8701ae62a0063499ccae37894
Alirezajhd <alirezajahed30@gmail.com>
2025-08-19 12:46:23 +0330

    fix hotel card

--- lib/features/home/widgets/hotel_card.dart
+++ lib/features/home/widgets/hotel_card.dart
@@ -22,47 +22,78 @@ class HotelCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Stack(children: []),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(width: 8),
-                Text(
-                  "${hotel.city}, ${hotel.country}",
-                  style: TextStyle(color: Colors.grey.shade700),
-                ),
-                SizedBox(width: 4),
-                Icon(
-                  Icons.location_on,
-                  color: Theme.of(context).colorScheme.primary,
-                  size: 18,
-                ),
-                SizedBox(width: 8),
-              ],
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: EdgeInsets.only(right: 8),
-              child: Text(
-                "از ${formatPrice(hotel.pricePerNight)} / شب",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: EdgeInsets.only(right: 8, left: 8),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "مشاهده و انتخاب اتاق",
-                    style: TextStyle(color: Colors.white),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      SizedBox(width: 8),
+                      Icon(Icons.star, color: Colors.amber, size: 20),
+                      SizedBox(width: 4),
+                      Text(
+                        "${hotel.rating} (${formatPrice(hotel.reviewCount)})",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Spacer(),
+                      Text(
+                        hotel.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                    ],
                   ),
-                ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(width: 8),
+                      Text(
+                        "${hotel.city}, ${hotel.country}",
+                        style: TextStyle(color: Colors.grey.shade700),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.location_on,
+                        color: Theme.of(context).colorScheme.primary,
+                        size: 18,
+                      ),
+                      SizedBox(width: 8),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: EdgeInsets.only(right: 8),
+                    child: Text(
+                      "از ${formatPrice(hotel.pricePerNight)} / شب",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: EdgeInsets.only(right: 8, left: 8),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "مشاهده و انتخاب اتاق",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                ],
               ),
             ),
-            SizedBox(height: 8),
           ],
         ),
       ),
