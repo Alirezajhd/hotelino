83c17d0a030564e0ce01613bc3ad12fdf57d2e37
Alirezajhd <alirezajahed30@gmail.com>
2025-08-19 12:19:46 +0330

    add price formatter

--- lib/features/home/widgets/hotel_card.dart
+++ lib/features/home/widgets/hotel_card.dart
@@ -1,10 +1,39 @@
+import 'package:Hotelino/core/utils/price_formater.dart';
+import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/home/presentation/favoriteProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HotelCard extends StatelessWidget {
-  const HotelCard({super.key});
+  HotelCard({super.key, required this.hotel});
+  Hotel hotel;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final favoriteProvider = Provider.of<FavotireItemProvider>(context);
+    bool isFavorite = favoriteProvider.isFavorite(hotel.id);
+
+    return SizedBox(
+      width: 280,
+      child: Card(
+        elevation: 4,
+        margin: EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Stack(children: []),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(width: 8),
+                Icon(Icons.star, color: Colors.amber, size: 20),
+                SizedBox(width: 4),
+                Text("${hotel.rating} (${formatPrice(hotel.reviewCount)})"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
