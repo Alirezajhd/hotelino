a26bb1750432f3ec43905a378583641a727218cd
Alirezajhd <alirezajahed30@gmail.com>
2025-09-25 11:12:38 +0330

    add _formKey to the form in bookingPage.dart

--- lib/features/booking/presentation/booking_page.dart
+++ lib/features/booking/presentation/booking_page.dart
@@ -1,10 +1,48 @@
+import 'package:Hotelino/features/booking/presentation/booking_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class BookingPage extends StatelessWidget {
+class BookingPage extends StatefulWidget {
   const BookingPage({super.key});
 
   @override
+  State<BookingPage> createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  void resetForm() {
+    Future.delayed(Duration(milliseconds: 100));
+    _formKey.currentState?.reset();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'فرم رزرو هتل',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Consumer<BookingProvider>(
+            builder: (context, bookinProvider, child) {
+              return Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
