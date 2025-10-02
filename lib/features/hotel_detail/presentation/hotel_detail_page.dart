564d2dbcce79491acb3ee70b6345de5b52e82631
Alirezajhd <alirezajahed30@gmail.com>
2025-10-02 13:01:32 +0330

    add hotel_detail_page.dart and get data using FutureBuilder

---
+++ lib/features/hotel_detail/presentation/hotel_detail_page.dart
@@ -0,0 +1,27 @@
+import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/home/data/repositories/hotel_repositories.dart';
+import 'package:Hotelino/shared/services/json_data_services.dart';
+import 'package:flutter/material.dart';
+
+class HotelDetailPage extends StatelessWidget {
+  final String hotelId;
+  const HotelDetailPage({super.key, required this.hotelId});
+
+  @override
+  Widget build(BuildContext context) {
+    final hotelRepositorie = HotelRepositories(
+      jsonDataServices: JsonDataServices(),
+    );
+    final textTheme = Theme.of(context).textTheme;
+
+    return FutureBuilder<Hotel>(
+      future: hotelRepositorie.getHotelById(hotelId),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        final hotel = snapshot.data!;
+      },
+    );
+  }
+}
